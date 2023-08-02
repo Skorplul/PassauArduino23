b8e989c8980e271137a3e481b85afc9a96a890e4
Skorp <105490480+Skorplul@users.noreply.github.com>
2023-08-02 17:23:23 +0200

    Add files via upload

--- BongoCat/BongoCat.ino
+++ BongoCat/BongoCat.ino
@@ -319,13 +319,13 @@ void TEQUILA() {
     // notes in the melody:
   int melody[] = {
     NOTE_F3, NOTE_F3, NOTE_ES3, NOTE_G3, NOTE_E3, NOTE_F3, NOTE_C3, PAUSE, NOTE_F3, NOTE_F3, NOTE_ES3, NOTE_G3, NOTE_E3, NOTE_F3, PAUSE, NOTE_D3, NOTE_E3, NOTE_G3, NOTE_B3, NOTE_G3, PAUSE, NOTE_G3, PAUSE, NOTE_D3, NOTE_E3, NOTE_G3, 
-    NOTE_B3, NOTE_G3, PAUSE, NOTE_G3, PAUSE, NOTE_D3, NOTE_E3, NOTE_G3, NOTE_B3, NOTE_F3, NOTE_A3, NOTE_A3, NOTE_A3, NOTE_A3, NOTE_A3, NOTE_A3, PAUSE, 
+    NOTE_B3, NOTE_G3, PAUSE, NOTE_G3, PAUSE, NOTE_D3, NOTE_E3, NOTE_G3, NOTE_B3, NOTE_G3, NOTE_A3, NOTE_A3, NOTE_A3, NOTE_A3, NOTE_A3, NOTE_A3, PAUSE, 
     NOTE_D3, 
   };
 
   // note durations: 4 = quarter note, 8 = eighth note, etc.:
   int noteDurations[] = {
-    4, 8, 8, 8, 4, 4, 8, 2, 4, 8, 8, 8, 8, 4, 2, 8, 8, 8, 2, 4, 8, 8, 8, 8, 8, 8, 2, 4, 8, 8, 8, 8, 8, 8, 2, 2, 8, 8, 8, 8, 4, 4, 8, 8,
+    4, 8, 8, 8, 4, 4, 8, 2, 4, 8, 8, 8, 8, 4, 2, 8, 8, 8, 2, 4, 8, 8, 8, 8, 8, 8, 2, 4, 8, 8, 8, 8, 8, 8, 2, 2, 8, 8, 8, 8, 4, 4, 16, 8,
   };
   
   
