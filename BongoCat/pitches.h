6ed0cf6b1a0331f4bd48963188f165d1eaa851d0
Skorp <105490480+Skorplul@users.noreply.github.com>
2023-08-02 17:15:38 +0200

    Add files via upload

--- BongoCat/pitches.h
+++ BongoCat/pitches.h
@@ -2,6 +2,8 @@
    Public Constants
  *************************************************/
 
+
+#define PAUSE 0
 #define NOTE_B0 31
 #define NOTE_C1 33
 #define NOTE_CS1 35
@@ -31,7 +33,8 @@
 #define NOTE_CS3 139
 #define NOTE_D3 147
 #define NOTE_DS3 156
-#define NOTE_E3 165
+#define NOTE_E3 158
+#define NOTE_ES3 155
 #define NOTE_F3 175
 #define NOTE_FS3 185
 #define NOTE_G3 196
